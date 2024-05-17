fa559f46f2a74fbf6ac5bc02303343d317a79719
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-05-17 21:28:16 +0900

    ✨ feat: 소모임 즐겨찾기 기능 개발
    소모임 즐겨찾기 시
    1. 이미 즐겨찾기 된 경우 AlreadyLikeException 예외를 발생시킴
    2. 즐겨찾기시 소모임 즐겨찾기 엔티티 생성

--- src/main/java/com/junghun/common/domain/like/service/LikeClubGatheringService.java
+++ src/main/java/com/junghun/common/domain/like/service/LikeClubGatheringService.java
@@ -4,6 +4,7 @@ import com.junghun.common.domain.gathering.entity.ClubGathering;
 import com.junghun.common.domain.gathering.service.ClubGatheringService;
 import com.junghun.common.domain.like.dto.LikeClubGatheringDto;
 import com.junghun.common.domain.like.entity.LikeClubGathering;
+import com.junghun.common.domain.like.exception.AlreadyLikeException;
 import com.junghun.common.domain.like.repository.LikeClubGatheringRepository;
 import com.junghun.common.domain.user.entity.User;
 import com.junghun.common.domain.user.service.UserService;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +23,8 @@ public class LikeClubGatheringService {
     private final ClubGatheringService gatheringService;
 
     public LikeClubGathering like(LikeClubGatheringDto likeClubGatheringDto) {
-        repository.
+        List<LikeClubGathering> likeList = repository.findByUserIdAndClubGatheringId(likeClubGatheringDto.getUserId(),likeClubGatheringDto.getGatheringId());
+        if(!likeList.isEmpty()) throw new AlreadyLikeException("이미 즐겨찾기 중입니다.");
 
         User user = userService.findById(likeClubGatheringDto.getUserId());
         LocalDateTime writeDate = LocalDateTime.now();
