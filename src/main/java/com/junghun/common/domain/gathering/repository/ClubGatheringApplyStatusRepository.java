219c26215a0e2d4e46c78f1b6f4fa2963669bb34
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-05-20 20:06:27 +0900

    ✨ feat: 소모임시 최대 인원에 따른 참여신청 분기처리
    현재 승인된 지원에 대한 수를 조회 후 capacity와 비교하며 신청이 가능한지 판단하고 그에 따른 분기 처리 작업

--- src/main/java/com/junghun/common/domain/gathering/repository/ClubGatheringApplyStatusRepository.java
+++ src/main/java/com/junghun/common/domain/gathering/repository/ClubGatheringApplyStatusRepository.java
@@ -3,6 +3,7 @@ package com.junghun.common.domain.gathering.repository;
 import com.junghun.common.domain.gathering.entity.ClubGatheringApplyStatus;
 import com.junghun.common.domain.gathering.entity.OneDayGatheringApplyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,9 @@ import java.util.List;
 public interface ClubGatheringApplyStatusRepository extends JpaRepository<ClubGatheringApplyStatus, Long> {
 
     List<ClubGatheringApplyStatus> findByApplierIdAndClubGatheringId(Long applierId, Long clubGatheringId);
+
+    @Query("SELECT ca FROM ClubGatheringApplyStatus ca " +
+            "WHERE ca.id = :clubGatheringId " +
+            "AND ca.status = true")
+    List<ClubGatheringApplyStatus> findApprovedApplies(Long clubGatheringId);
 }
--- src/main/java/com/junghun/common/domain/gathering/service/ClubGatheringApplyStatusService.java
+++ src/main/java/com/junghun/common/domain/gathering/service/ClubGatheringApplyStatusService.java
@@ -2,8 +2,11 @@ package com.junghun.common.domain.gathering.service;
 
 import com.junghun.common.domain.gathering.entity.ClubGathering;
 import com.junghun.common.domain.gathering.entity.ClubGatheringApplyStatus;
+import com.junghun.common.domain.gathering.entity.OneDayGatheringApplyStatus;
 import com.junghun.common.domain.gathering.exception.AlreadyApplyGatheringException;
+import com.junghun.common.domain.gathering.exception.FullGatheringException;
 import com.junghun.common.domain.gathering.exception.NotFoundGatheringApplyStatusException;
+import com.junghun.common.domain.gathering.exception.NotProvideServiceLocationException;
 import com.junghun.common.domain.gathering.repository.ClubGatheringApplyStatusRepository;
 
 import com.junghun.common.domain.user.entity.User;
@@ -23,14 +26,23 @@ public class ClubGatheringApplyStatusService {
 
     public void applyGathering(Long applierId, Long clubGatheringId) {
 
-        List<ClubGatheringApplyStatus> oneDayGatheringApplyStatusList = repository.findByApplierIdAndClubGatheringId(applierId, clubGatheringId);
+        List<ClubGatheringApplyStatus> clubGatheringApplyStatusList = repository.findByApplierIdAndClubGatheringId(applierId, clubGatheringId);
 
-        if (!oneDayGatheringApplyStatusList.isEmpty()) {
+        if (!clubGatheringApplyStatusList.isEmpty()) {
             throw new AlreadyApplyGatheringException("이미 신청중이거나, 승인된 모임입니다.");
         }
 
+        List<ClubGatheringApplyStatus> approvedApplies = repository.findApprovedApplies(clubGatheringId);
+
         User applier = userService.findById(applierId);
         ClubGathering clubGathering = clubGatheringService.findById(clubGatheringId);
+
+        if (clubGathering.getCapacity() <= approvedApplies.size()) {
+            throw new FullGatheringException("참여자가 많아 더이상 참여신청을 할 수 없습니다.");
+        }
+        if (!clubGathering.getCityList().contains(applier.getLocation().get("city"))) {
+            throw new NotProvideServiceLocationException("현재 이용자의 위치에서는 소모임에 참여할 수 없습니다.");
+        }
 
         ClubGatheringApplyStatus clubGatheringApplyStatus = ClubGatheringApplyStatus.builder()
                 .applier(applier)
